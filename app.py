58fb93012ca5bea6563e62fea4aa4d9ea8b98e51
virajdeshaval <deshaval.viraj@gmail.com>
2021-05-30 15:09:49 +0530

    feat: Lists added to View     - All the lists are added to view.     - Views can be clicked to display todos.     - List title is added to the todo items.

--- app.py
+++ app.py
@@ -83,6 +83,13 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+    lists = TodoList.query.all(),
+    active_list = TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
